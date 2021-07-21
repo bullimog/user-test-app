15d0500ed33716ad094b881a18d5c460fbdf85db
bullimog <gmb@bullimore.com>
2021-07-21 16:28:32 +0100

    additional ExceptionHandler

--- src/main/java/com/bullimore/usertestapp/controllers/ErrorController.java
+++ src/main/java/com/bullimore/usertestapp/controllers/ErrorController.java
@@ -4,12 +4,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 
 @RestControllerAdvice
 public class ErrorController {
+    @ExceptionHandler(WebClientRequestException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleFetchException(){
+        return "Sorry, there was a problem when fetching data. Please report this error and try again later.";
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleAllExceptions(){
-        return "Sorry, there was an internal problem when fetching data. Please try again later.";
+        return "Sorry, there was an internal problem. Please try again later.";
     }
 }
